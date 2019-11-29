939212e903707abd86d30ac59d50a61a77df3884
Svanderest <sebastian.vanderest@gmail.com>
2019-11-28 20:27:34 -0500

    Initial commit

---
+++ PROG1197Lab4/PROG1197Lab4Objects/MovieNode.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PROG1197Lab4Objects
+{
+    public class MovieNode : IComparable<MovieNode>, IEquatable<MovieNode>
+    {
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public TimeSpan Runtime { get; set; }
+        public string Director { get; set; }
+        public float Rating { get; set; }
+
+        private MovieNode right;
+        private MovieNode left;
+
+        public MovieNode Parent { get; private set; }
+        public MovieNode Right
+        {
+            get { return right; }
+            set
+            {
+                value.Parent = this;
+                right = value;
+            }
+        }
+
+        public MovieNode Left
+        {
+            get { return left; }
+            set
+            {
+                value.Parent = this;
+                left = value;
+            }
+        }
+
+        public List<MovieNode> Children
+        {
+            get
+            {
+                List<MovieNode> result = new List<MovieNode>();
+                if (right != null)
+                {
+                    result.Add(right);
+                    result.AddRange(right.Children);
+                }
+                if (left != null)
+                {
+                    result.Add(left);
+                    result.AddRange(left.Children);
+                }
+                return result;
+            }
+        }
+
+        public bool Inbalanced
+        {
+            get
+            {
+                return (left == null ^ right == null) || (right?.Inbalanced ?? false) || (left?.Inbalanced ?? false);
+            }
+        }
+
+        public int CompareTo(MovieNode other)
+        {
+            if (this.ReleaseDate < other.ReleaseDate)
+                return -1;
+            else if (this.ReleaseDate > other.ReleaseDate)
+                return 1;
+            else
+                return this.Title.CompareTo(other.Title);
+        }
+
+        public bool Equals(MovieNode other)
+        {
+            return this.Title == other.Title
+                && this.ReleaseDate == other.ReleaseDate
+                && this.Runtime == other.Runtime
+                && this.Director == other.Director
+                && this.Rating == other.Rating;
+        }
+    }
+}
---
+++ PROG1197Lab4/PROG1197Lab4Objects/MovieTree.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace PROG1197Lab4Objects
+{
+    public class MovieTree
+    {
+        private static MovieTree tree;
+
+        public static MovieTree Create()
+        {
+            tree = new MovieTree();
+            return tree;
+        }
+
+        public MovieNode Root { get; private set; }
+
+        public MovieNode Build(IEnumerable<MovieNode> nodes)
+        {
+            nodes = nodes.OrderBy(n => n);
+            var current = nodes.ElementAt(nodes.Count() / 2);
+            if (current != nodes.Max())
+                current.Right = Build(nodes.Where(n => n.CompareTo(current) == 1));
+            if (current != nodes.Min())
+                current.Left = Build(nodes.Where(n => n.CompareTo(current) == -1));
+            return current;
+        }
+
+        public void Add(MovieNode item)
+        {
+            if (Root == null)
+                Root = item;
+            else if (!Root.Inbalanced)
+            {
+                var current = Root;
+                while(current.CompareTo(item) != 0)
+                {
+                    int i = item.CompareTo(current);
+                    if (i == -1 && current.Left == null)
+                        current.Left = item;
+                    if (i == 1 && current.Right == null)
+                        current.Right = item;
+                    current = i == 1 ? current.Right : current.Left;
+                }
+            }            
+            else
+            {
+                var current = Root;
+                while(true)
+                {
+                    var next = item.CompareTo(current) == -1 ? current.Left : current.Right;
+                    if(next == null || !next.Inbalanced)
+                    {
+                        float i = current.CompareTo(Root);
+                        MovieNode previous = null;
+                        if (i != 0)
+                        {
+                            i = current.CompareTo(current.Parent.Left) - 0.5f;
+                            previous = current.Parent;
+                        }
+                        var rebuiltTree = Build(current.Children.Union(new List<MovieNode> { current, item }));
+                        if (i == 0)
+                            Root = rebuiltTree;
+                        else if (i < 0)
+                            previous.Left = rebuiltTree;
+                        else
+                            previous.Right = rebuiltTree;
+                        break;
+                    }                 
+                }
+            }            
+        }
+    }
+}
